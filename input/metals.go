19c5b45d5ce40e51c1fb051d6d388350c7028441
Galih Pratama <galih.pratama2207@gmail.com>
2019-08-18 07:37:42 +0700

    Refactored insertMetals function

--- input/metals.go
+++ input/metals.go
@@ -2,11 +2,55 @@ package input
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/ggalihpp/galactic-market/roman"
 	"github.com/ggalihpp/galactic-market/stored"
 )
+
+func insertMetals(input string) string {
+	var err error
+	var mtrl, rom string
+	var crdts, mtrlIdx, qty int
+
+	msgs := strings.Split(input, " ")
+
+	for i := range msgs {
+		if msgs[i] == "IS" {
+			mtrlIdx = i - 1
+			mtrl = msgs[i-1]
+			crdts, err = strconv.Atoi(msgs[i+1])
+			if err != nil {
+				fmt.Println("ERORRR:: ", err.Error())
+				return haveNoIdea()
+			}
+		}
+	}
+
+	rmns := msgs[0:mtrlIdx]
+
+	for _, v := range rmns {
+		if val, ok := stored.RulesRoman.CheckValue(v); ok {
+			rom += val
+		} else {
+
+			return haveNoIdea()
+		}
+	}
+
+	qty, err = roman.Rtoi(rom)
+	if err != nil {
+		fmt.Println("ERORRR:: ", err.Error())
+		return haveNoIdea()
+	}
+
+	//// evaluate the value
+	value := float64(crdts) / float64(qty)
+	stored.MetalsValue[mtrl] = value
+
+	return fmt.Sprintf("%v value updated:: %v", strings.Title(mtrl), value)
+}
 
 func getMetalsValue(msg string) string {
 	var rom, material string
